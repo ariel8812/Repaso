255427f9be162c2509dde38a75ef1505b9a7789a
lia07 <sarzurisilvestre@example.com>
2019-11-25 17:38:02 +0100

    cambios suma

--- calculadora.ts
+++ calculadora.ts
@@ -1,16 +1,16 @@
 //funciones de una calculadora
 function suma(n1,n2){
-    // var n1 = 10;
-    // var n2 = 7;
+   
     var suma = n1 + n2;
+    return suma;
     
-    console.log(suma(5,7));
+}
 function residuo(x, y) {
     return x % y;
 
 }
 //funciones de una calculadora}
-}
+
 function resta(n1,n2){
  var rs = n1 - n2;
  return rs;

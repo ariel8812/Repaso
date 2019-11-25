59589f3b3c7ff2aa385ea74e38eb855ce430e8af
giovi7 <giovi7azul@gmail.com>
2019-11-25 12:20:05 -0400

    resta

--- calculadora.ts
+++ calculadora.ts
@@ -1,19 +1,14 @@
 //funciones de una calculadora
-<<<<<<< HEAD
 function sumar(){
     var n1 = 10;
     var n2 = 7;
     var suma = 0;
     suma = n1 + n2;
     console.log(suma);
-=======
-function resta(){
-  var n1 = 8;
-  var n2 = 4;
-  var res= 0;
-  res = n1 - n2;
-  console.log(resta);
->>>>>>> a5158612fd88f37f666e75e579264b9b54308d58
+function resta(n1,n2){
+ var res = n1 -n2;
+ return res;
+}
 //funciones de una calculadora}
 
 function division(n1,n2){
